e18c5fda016700f5fe464213c526357ee4713a91
James <develop@kuusisto.com>
2021-05-17 16:07:05 -0700

    First Commit

---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+using System;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // -- Arrange
+            Customer customer = new Customer()
+            {
+                FirstName = "Jamie",
+                LastName = "Kuusisto"
+            };
+
+            string expected = "Kuusisto, Jamie";
+
+            // -- Act
+            string actual = customer.FullName;
+
+            // -- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            // -- Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Kuusisto"
+            };
+            string expected = "Kuusisto";
+
+            // -- Act
+            string actual = customer.FullName;
+
+            // -- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            Customer customer = new Customer
+            {
+                FirstName = "Jamie"
+            };
+            string expected = "Jamie";
+
+            // -- Act
+            string actual = customer.FullName;
+
+            // -- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            // -- Arrange
+            var c1 = new Customer();
+            c1.FirstName = "Bilbo";
+            Customer.InstantCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Frodo";
+            Customer.InstantCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Rosie";
+            Customer.InstantCount += 1;
+
+            // -- Act
+
+            // -- Assert
+            Assert.AreEqual(3, Customer.InstantCount);
+        }
+    }
+    
+}
---
+++ Tests/ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84b9b818-d69c-4a16-afd2-7f876ac0b6f6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
