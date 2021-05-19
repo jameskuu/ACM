99d920674575ac142e9a4dca8b1cd197a9dfd07a
James <develop@kuusisto.com>
2021-05-18 20:36:33 -0700

    Created the CustomerRepository class

--- ACM/Address.cs
+++ ACM/Address.cs
@@ -8,5 +8,41 @@ namespace ACM
 {
     class Address
     {
+        public Address ()
+        {
+
+        }
+
+        public Address (int addressId)
+        {
+            AddressId = addressId;
+        }
+
+        public int AddressId { get; private set; }
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string AddressType { get; set; }
+
+        /// <summary>
+        /// Validates address
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (PostalCode == null) isValid = false;
+            //if (!string.IsNullOrWhiteSpace(StreetLine1)) isValid = false;
+            //if (!string.IsNullOrWhiteSpace(City)) isValid = false;
+            //if (!string.IsNullOrWhiteSpace(State)) isValid = false;
+            //if (!string.IsNullOrWhiteSpace(Country)) isValid = false;
+            //if (!string.IsNullOrWhiteSpace(AddressType)) isValid = false;
+
+            return isValid;
+        }
     }
 }
