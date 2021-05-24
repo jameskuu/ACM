8ebf3f966010554aed9e306a96ea9758b2ac56f2
James <develop@kuusisto.com>
2021-05-24 13:02:25 -0700

    added the order Repository and Unit test

--- ACM/Order.cs
+++ ACM/Order.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ACM.BL
 {
-    class Order
+    public class Order
     {
         public Order()
         {
@@ -19,6 +19,7 @@ namespace ACM.BL
         }
         public DateTimeOffset? OrderDate { get; set; }
         public int OrderId { get; private set; }
+        
 
         /// <summary>
         /// Validates the order infomation
@@ -32,34 +33,5 @@ namespace ACM.BL
 
             return isValid;
         }
-
-        /// <summary>
-        /// Retrieve one order
-        /// </summary>
-        /// <returns></returns>
-        public Order Retrieve(int orderId)
-        {
-            return new Order();
-        }
-
-
-        /// <summary>
-        /// Retrieve all orders
-        /// </summary>
-        /// <returns></returns>
-        public List<Order> Retrieve()
-        {
-            return new List<Order>();
-        }
-
-        /// <summary>
-        /// Saves the current Order
-        /// </summary>
-        /// <returns></returns>
-        public bool Save()
-        {
-            return true;
-        }
-
     }
 }
